f874fdce7016f7a5bf79254a992b68de78c4c858
tonkaxxx <tonkaxxx@mail.ru>
2025-06-02 16:12:58 +0300

    sec upd

--- minikube-start.sh
+++ minikube-start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cd k8s
+export $(grep -v '^#' .env | xargs)
+cd ..
+envsubst < k8s/mongo-secret.template.yaml > k8s/mongo-secret.yaml
 
 kubectl apply -f k8s/mongo-secret.yaml
 kubectl apply -f k8s/mongo-config.yaml
